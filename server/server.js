6da61f3ac10f3273c9797babcdda3af9e1dd79b6
Septusix <jahleelnemuel@gmail.com>
2026-04-06 14:22:01 +0800

    feat: update CORS configuration to include CLIENT_URL

--- server/server.js
+++ server/server.js
@@ -23,7 +23,10 @@ const port = process.env.PORT || 8001;
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+        "http://localhost:5173",
+        process.env.CLIENT_URL
+    ],
     credentials: true
 }));
 
